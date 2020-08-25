b81a4fdc81b199ad4a2a2a8480cdd79839632fc1
Utkarsh Bajpai <utkarsh.bajpai@oracle.com>
2020-08-25 20:44:28 +0530

    fix unexpected issue

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + '/dist/LiveStream')));
 
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/LiveStream/index.html'));   
-});  
+
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
